c68751fcbbebc58964214fdd70d332ac36720c9e
Ritu Kirsur <ritukirsur@gmail.com>
2024-11-03 11:44:55 -0800

    can create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,25 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user.username
+    @post.comments = 0
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
