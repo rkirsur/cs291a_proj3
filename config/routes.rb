005f64b842b71c2d84b5054adf79318cbfb3c1b6
Ritu Kirsur <ritukirsur@gmail.com>
2024-11-04 11:49:06 -0800

    logout button route changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
   resource :session
   resource :user
 
   get "/login", to: "users#new"
   post "/login", to: "users#create"
   get "/logout", to: "sessions#logout"
-  # post "/logout", to: "sessions#destroy"
-
-  # get "/posts/:id", to: "posts#show"
+  delete "/logout", to: "sessions#destroy"
 
   # Defines the root path route ("/")
   root "posts#index"
